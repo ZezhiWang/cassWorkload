ae793a4c1cde74818fa056d79d9ec46df32886e1
Ubuntu <wanganh@bc.edu>
2018-06-15 15:08:56 +0000

    added total time

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 all:
 	go build *.go
-run:
-	./workload 1000
+run1:
+	./workload 1000 102400
+run3:
+	./workload 1000 307200
+run5:
+	./workload 1000 502000
 clear:
 	rm workload
 
--- workload.go
+++ workload.go
@@ -5,7 +5,7 @@ import(
 	"strconv"
 	"fmt"
 	"time"
-	"log"
+//	"log"
 	"sync"
 //	"math/rand"
 	// go get github.com/gocql/gocql
@@ -14,13 +14,14 @@ import(
 
 // initialize mutex lock
 var mutex = &sync.Mutex{}
+var data_size int
 
 // write info into table before read, with out tracking time
 func initWrite(num int, session *gocql.Session){
 	// write data in the form (string,blob) into table tmp
 	arg := fmt.Sprintf("INSERT INTO tmp (key,val) values (?, ?)")
 	if err := session.Query(arg, string(num),make([]byte, 4)).Exec(); err != nil {
-		log.Fatal(err)
+//		log.Fatal(err)
 	}
 }
 
@@ -30,8 +31,8 @@ func write(num int, session *gocql.Session, wTime chan time.Duration){
 	arg := fmt.Sprintf("INSERT INTO tmp (key,val) values (?, ?)")
 	mutex.Lock()
 	start := time.Now()
-	if err := session.Query(arg,string(num),make([]byte, 4)).Exec(); err != nil {
-		log.Fatal(err)
+	if err := session.Query(arg,string(num),make([]byte, data_size)).Exec(); err != nil {
+		//log.Fatal(err)
 	}
 	end := time.Now()
 	mutex.Unlock()
@@ -45,11 +46,11 @@ func read(num int, session *gocql.Session, rTime chan time.Duration){
 	var key string
 	var val []byte
 	// write data in the form table tmp with key = num
-	arg := fmt.Sprintf("SELECT key,val FROM tmp WHERE key='%s'",string(num))
+	arg := fmt.Sprintf("SELECT key,val FROM tmp WHERE key=?")
 	mutex.Lock()
 	start := time.Now()
-	if err := session.Query(arg).Scan(&key,&val); err != nil {
-		log.Fatal(err)
+	if err := session.Query(arg,string(num)).Scan(&key,&val); err != nil {
+		//log.Fatal(err)
 	}
 	end := time.Now()
 	mutex.Unlock()
@@ -60,20 +61,25 @@ func read(num int, session *gocql.Session, rTime chan time.Duration){
 
 // delete value by key from table
 func delete(num int, session *gocql.Session){
-	arg := fmt.Sprintf("DELETE FROM tmp WHERE key='%s'",string(num))
-	if err := session.Query(arg).Exec(); err != nil {
-		log.Fatal(err)
+	arg := fmt.Sprintf("DELETE FROM tmp WHERE key=?")
+	if err := session.Query(arg,string(num)).Exec(); err != nil {
+		//log.Fatal(err)
 	}
 }
 
 func main(){
 	// command line arg -> number of read & write
 	num,_ := strconv.Atoi(os.Args[1])
+	data_size,_ = strconv.Atoi(os.Args[2])
 	// init cluster
-	cluster := gocql.NewCluster("172.17.0.4")
+	cluster := gocql.NewCluster("128.52.162.124","128.52.162.125","128.52.162.131","128.52.162.127","128.52.162.122","128.52.162.120")
 	// set keyspace to demo
 	cluster.Keyspace = "demo"
-	session,_ := cluster.CreateSession()
+	session,err := cluster.CreateSession()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(session)
+	}
 	defer session.Close()
 
 	wTime := make(chan time.Duration)
@@ -84,6 +90,8 @@ func main(){
 	for i := 0; i < num; i++{
 		initWrite(i, session)
 	}
+	
+	startTime := time.Now()
 
 	// go routine for concurrent read & write
 	for i := 0; i < num; i++{
@@ -96,6 +104,10 @@ func main(){
 		WTotal += int(<-wTime/time.Millisecond)
 		RTotal += int(<-rTime/time.Millisecond)
 	}
+	
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime) 
+	total := int(elapsedTime/time.Millisecond)
 
 	// clear table
 	for i := 0; i < num; i++{
@@ -104,4 +116,5 @@ func main(){
 
 	fmt.Printf("Avg write time: %f ms\n", float64(WTotal)/float64(num))
 	fmt.Printf("Avg read time: %f ms\n", float64(RTotal)/float64(num))
+	fmt.Printf("Total time: %d s\n", total/1000)
 }
